9a543e9dedafa442ba057961d837c807e3ad57f5
Hitesh Sharma <hiteshsharmahs0987@gmail.com>
2023-01-13 17:37:45 +0530

    sett

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports={
+  //  SENDGRID_API:"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",
+    MONGOURI:"mongodb+srv://hiteshsharma:heloiamtheboss@cluster0.tm9sgfk.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"hfuahhf",
+    EMAIL:"http:localhost:3000/"
+}
+//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  -- new
+//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
